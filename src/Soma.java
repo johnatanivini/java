59641dd54eccce032ac1a85fce773e25427f5919
ivini <johnatan.ivini@gmail.com>
2021-12-03 12:23:28 -0300

    Ajustando a questão da virgula

--- src/Soma.java
+++ src/Soma.java
@@ -4,8 +4,12 @@ import java.util.Locale;
 public class Soma {
     public static void main(String[] args)
     {
+        Locale.setDefault(new Locale("br", "pt-BR"));
         String valor1 = JOptionPane.showInputDialog("Adicione o primeiro número");
         String valor2 = JOptionPane.showInputDialog("Adicione o segundo número");
+
+        valor1 = valor1.replace(',', '.');
+        valor2 = valor2.replace(',', '.');
 
         double numero1 = Double.parseDouble(valor1);
         double numero2 = Double.parseDouble(valor2);
